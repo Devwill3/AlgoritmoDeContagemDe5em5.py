d29e99c535ca8142121a5284d602f13cec0c8a93
William Matheus <williammatheus591@gmail.com>
2023-08-22 11:25:37 -0300

    jogando AlgoritmoDeContagemDe5em5.py no github

---
+++ AlgoritmoDeContagemDe5em5.py
@@ -0,0 +1,8 @@
+# Agora faça com que o algoritmo do exercício anterior exiba 
+# a mesma contagem mas na ordem inversa.
+# Ex.: 50 45 40 35 30 25 … 5 0 Fim!
+
+for olup in range(51, 0, -5):
+    print(olup)
+    print(f"::")
+print(f"Mif!")
